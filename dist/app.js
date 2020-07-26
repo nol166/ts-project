024b197a98a2bd3d81d5ab5ce501c2577eacd6d6
John McCambridge <jmccamb@gmail.com>
2020-07-26 16:08:05 -0500

    render form to the dom

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,15 @@
 "use strict";
-console.log('test');
+class ProjectInput {
+    constructor() {
+        this.templateEl = document.getElementById('project-input');
+        this.hostEl = document.getElementById('app');
+        const importedHTML = document.importNode(this.templateEl.content, true);
+        this.element = importedHTML.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostEl.insertAdjacentElement('afterbegin', this.element);
+    }
+}
+const projInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,22 @@
-// Code goes here!
-console.log('test')
+class ProjectInput {
+    templateEl: HTMLTemplateElement
+    hostEl: HTMLDivElement
+    element: HTMLElement
+
+    constructor() {
+        this.templateEl = document.getElementById(
+            'project-input'
+        )! as HTMLTemplateElement
+        this.hostEl = document.getElementById('app')! as HTMLDivElement
+
+        const importedHTML = document.importNode(this.templateEl.content, true)
+        this.element = importedHTML.firstElementChild as HTMLFormElement
+        this.attach()
+    }
+
+    private attach() {
+        this.hostEl.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const projInput = new ProjectInput()
