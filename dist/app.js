0069cef6e66ec4165656293fd481392c67f03bdd
John McCambridge <jmccamb@gmail.com>
2020-07-26 16:24:32 -0500

    add autobind function

--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,48 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+const autoBind = (_target, _methodName, descriptor) => {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDescriptor;
+};
 class ProjectInput {
     constructor() {
         this.templateEl = document.getElementById('project-input');
         this.hostEl = document.getElementById('app');
         const importedHTML = document.importNode(this.templateEl.content, true);
         this.element = importedHTML.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInput = this.element.querySelector('#title');
+        this.descriptionInput = this.element.querySelector('#description');
+        this.peopleInput = this.element.querySelector('#people');
+        this.setUpListeners();
         this.attach();
+    }
+    submitHandler(e) {
+        e.preventDefault();
+        let titleVal = this.titleInput.value;
+        console.log('ProjectInput -> submitHandler -> titleVal', titleVal);
+    }
+    setUpListeners() {
+        this.element.addEventListener('submit', this.submitHandler);
     }
     attach() {
         this.hostEl.insertAdjacentElement('afterbegin', this.element);
     }
 }
+__decorate([
+    autoBind
+], ProjectInput.prototype, "submitHandler", null);
 const projInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
+// autobind decorator
+const autoBind = (
+    _target: any,
+    _methodName: string,
+    descriptor: PropertyDescriptor
+) => {
+    const originalMethod = descriptor.value
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this)
+            return boundFn
+        },
+    }
+    return adjDescriptor
+}
+// ProjectInput class
+
 class ProjectInput {
     templateEl: HTMLTemplateElement
     hostEl: HTMLDivElement
     element: HTMLElement
+    titleInput: HTMLInputElement
+    descriptionInput: HTMLInputElement
+    peopleInput: HTMLInputElement
 
     constructor() {
         this.templateEl = document.getElementById(
@@ -11,7 +32,34 @@ class ProjectInput {
 
         const importedHTML = document.importNode(this.templateEl.content, true)
         this.element = importedHTML.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        //  populate input fields
+        this.titleInput = this.element.querySelector(
+            '#title'
+        ) as HTMLInputElement
+        this.descriptionInput = this.element.querySelector(
+            '#description'
+        ) as HTMLInputElement
+        this.peopleInput = this.element.querySelector(
+            '#people'
+        ) as HTMLInputElement
+        this.setUpListeners()
         this.attach()
+    }
+
+    @autoBind
+    private submitHandler(e: Event) {
+        // get values
+        e.preventDefault()
+        let titleVal = this.titleInput.value
+        console.log('ProjectInput -> submitHandler -> titleVal', titleVal)
+
+        // validate them
+    }
+
+    private setUpListeners() {
+        this.element.addEventListener('submit', this.submitHandler)
     }
 
     private attach() {
